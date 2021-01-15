dbd71626f3b5ce575171549fa9adb0eb447a8ec9
Stefan Schneider <stefan.schneider@upb.de>
2021-01-15 22:55:17 +0100

    django heroku

--- pytorch_django/pytorch_django/prod_settings.py
+++ pytorch_django/pytorch_django/prod_settings.py
@@ -1,4 +1,6 @@
 """ Production Settings """
+import django_heroku
+
 # default: use settings from main settings.py if not overwritten
 from .settings import *
 
@@ -12,3 +14,7 @@ DEBUG = False
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 
 ALLOWED_HOSTS = ['pytorch-django.herokuapp.com']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
