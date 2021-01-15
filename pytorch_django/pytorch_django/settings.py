3618d9cce24d3ced180201b05145300ee3029730
Stefan Schneider <stefan.schneider@upb.de>
2021-01-15 22:36:20 +0100

    add whitenoise

--- pytorch_django/pytorch_django/settings.py
+++ pytorch_django/pytorch_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'image_classification.apps.ImageClassificationConfig',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
